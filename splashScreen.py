fc6df905911b0b1cb86fa7a32515550d5e232f74
Marcot-10 <marcot1003@icloud.com>
2025-01-30 20:50:45 -0600

    Update splashScreen.py

--- splashScreen.py
+++ splashScreen.py
@@ -1,1 +1,26 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class SplashScreen:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("800x600")
+
+        img = Image.open("logo.jpg")
+        img = img.resize((800, 600), Image.Resampling.LANCZOS) 
+
+        self.bg = ImageTk.PhotoImage(img)
+
+        self.label = tk.Label(self.root, image = self.bg)
+        self.label.place(x = 0, y = 0)
+
+        #close after 3 seconds
+
+        self.root.after(3000, self.root.destroy)
+
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    SplashScreen()
 
