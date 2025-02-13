44094768b733438a580838d8651abc602990fc2a
moblando <landorams1@gmail.com>
2025-02-13 14:54:58 -0600

    Update playerAction.py

--- playerAction.py
+++ playerAction.py
@@ -1,5 +1,6 @@
 import playerScreen
 import tkinter as tk
+
 #action menu
 #-----------------------------------
 def player_action_main(previous_window):
@@ -8,14 +9,22 @@ def player_action_main(previous_window):
 
 
     #currently displays black screen after both teams are filled
-    root = tk.Tk()
-    root.geometry("800x600")
-    root.title("Player Action Screen")
-    root.configure(bg="black")
+    action_window = tk.Tk()
+    action_window.title("Player Action")
+    action_window.geometry("800x600")
+    action_window.configure(bg="black")
+
+    #switch back to player Screen
+    def on_f1(event):
+        action_window.destroy()
+        playerScreen.playerScreen()
 
 
-    root.mainloop()
+    action_window.bind("<F1>", on_f1)
 
+    action_window.mainloop()
+
+#-----------------------------------
 
 
     #pass
