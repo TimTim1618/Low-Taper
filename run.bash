fd979590d6a5a6cf7f088e06c40377f3e5408d84
tyler1o9 <133690083+tyler1o9@users.noreply.github.com>
2025-02-16 22:24:24 -0600

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Select a network to connect to:"
-echo "1. Localhost (127.0.0.1:20001)"
+echo "1. Localhost (127.0.0.1:7501)"
 echo "2. Enter custom network"
 read -p "Enter your choice (1 or 2): " choice
 
 if [ "$choice" = "1" ]; then
     ip="127.0.0.1"
-    port=20001
+    port=7501
 elif [ "$choice" = "2" ]; then
     read -p "Enter IP address: " ip
     read -p "Enter Port number: " port
 else
     echo "Invalid choice, defaulting to localhost."
     ip="127.0.0.1"
-    port=20001
+    port=7501
 fi
 
 echo "Selected Network: $ip:$port"
